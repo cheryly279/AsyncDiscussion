27e186238c03a07284c360954132845db02edf30
Cheryl Yaeger <cheryl.yaeger@gmail.com>
2015-09-26 10:34:17 -0400

    Promise all example.

---
+++ Promises/10_promise_all.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var fs_converted = require('./fs_converted');
+
+fs_converted.readdir('sample_files').then(function (files) {
+    return Promise.all(files.map(function (file) {
+        return fs_converted.readFile(path.join(__dirname, 'sample_files', file));
+    }));
+}).then(function (dataArr) {
+    return Promise.all(dataArr.map(function (data) {
+        console.log('file: ' + data.file);
+        console.log('contents: ' + data.contents);
+        return fs_converted.writeFile(data.file + '_bkp', data.contents, 'utf8');
+    }));
+}).then(function () {
+    console.log('backups saved.');
+}).catch(function (err) {
+    console.log('Error: ' + err);
+})
